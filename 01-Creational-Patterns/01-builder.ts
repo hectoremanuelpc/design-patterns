6f7e2d76209a2b5124d2742d3cec492ad7be7f31
Emanuel Peña <emanuel.cuevas03@gmail.com>
2025-01-25 12:59:42 +0100

    Builder Pattern Example

--- 01-Creational-Patterns/01-builder.ts
+++ 01-Creational-Patterns/01-builder.ts
@@ -12,3 +12,70 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+    public cpu: string = "cpu - not defined";
+    public ram: string = "ram - not defined";
+    public storage: string = "storage - not defined";
+    public gpu?: string;
+
+    displayConfig(): void {
+        console.log(`Computer Configuration
+          CPU: ${this.cpu}
+          RAM: ${this.ram}
+          Storage: ${this.storage}
+          GPU: ${this.gpu ?? "No GPU"}
+          `);
+    }
+}
+
+class ComputerBuilder {
+    private computer: Computer;
+
+    constructor() {
+        this.computer = new Computer();
+    }
+
+    setCPU(cpu: string): ComputerBuilder {
+        this.computer.cpu = cpu;
+        return this;
+    }
+
+    setRAM(ram: string): ComputerBuilder {
+        this.computer.ram = ram;
+        return this;
+    }
+    setStorage(storage: string): ComputerBuilder {
+        this.computer.storage = storage;
+        return this;
+    }
+    setGPU(gpu: string): ComputerBuilder {
+        this.computer.gpu = gpu;
+        return this;
+    }
+
+    build(): Computer {
+        return this.computer;
+    }
+}
+
+function main() {
+    const basicComputer = new ComputerBuilder().setCPU("M2 Max").setRAM("32GB").setStorage("512GB").build();
+    console.log("\n%cbasicComputer:", COLORS.blue);
+    basicComputer.displayConfig();
+
+    const gamingComputer = new ComputerBuilder()
+        .setCPU("i9-17390KF")
+        .setRAM("32GB")
+        .setRAM("24GB")
+        .setStorage("512GB")
+        .setStorage("1TB")
+        .setGPU("Nvidia RTX 4090")
+        .build();
+
+    console.log("%cgamingComputer:", COLORS.red);
+    gamingComputer.displayConfig();
+}
+main();
